e964df08eabc53b82a32bcc60f4054216476e466
alan <cvinny33@gmail.com>
2022-10-09 15:47:22 -0600

    started adding seed values

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,24 @@
-DROP DATABASE IF EXISTS inventory_db;
-CREATE DATABASE inventory_db;
-USE inventory_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
 );
 
 CREATE TABLE role(
-    id INT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT
 );
 
 CREATE TABLE employee(
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
 );
     
