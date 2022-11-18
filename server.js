a7a8f7aeb1d411217cf9bf1251e344591647d76b
alan <cvinny33@gmail.com>
2022-11-17 18:24:05 -0700

    created function to pull all roles

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const menu = async () => {
         type: 'list',
         name: 'answer',
         message: 'Choose one',
-        choices: ['View all departments', 'View all employees']
+        choices: ['View all departments', 'View all employees', 'View all roles']
     }])
     switch (answer) {
         case 'View all departments':
@@ -13,6 +13,9 @@ const menu = async () => {
             break
         case 'View all employees':
             viewAllEmployees()
+            break
+        case 'View all roles':
+            viewAllRoles()
             break
     }
 }
@@ -29,6 +32,16 @@ const viewAllDepartments = async () => {
 const viewAllEmployees = async () => {
     try {
         const [data] = await db.promise().query('SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, conCat(manager.first_name, " " , manager.last_name) as manager from employee left join role on employee.role_id = role.id left join department on role.department_id = department.id left join employee manager on employee.manager_id = manager.id ')
+        console.table(data);
+        menu()
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const viewAllRoles = async () => {
+    try {
+        const[data] = await db.promise().query('SELECT role.title, role.salary,department.name from role left join department on role.department_id = department.id')
         console.table(data);
         menu()
     } catch (err) {
