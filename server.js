bf37a9a197f5a6d9c0b1dfabde181aed3c7acf89
alan <cvinny33@gmail.com>
2022-11-20 12:29:25 -0700

    created role, department, and employee functions.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const menu = async () => {
         type: 'list',
         name: 'answer',
         message: 'Choose one',
-        choices: ['View all departments', 'View all employees', 'View all roles']
+        choices: ['View all departments', 'View all employees', 'View all roles', 'Create department', 'Create employee', 'Create role']
     }])
     switch (answer) {
         case 'View all departments':
@@ -17,11 +17,30 @@ const menu = async () => {
         case 'View all roles':
             viewAllRoles()
             break
+        case 'Create department':
+            createDepartment()
+            break
+        case 'Create employee':
+            createEmployee
+            break
+        case 'Create role':
+            createRole
+            break
     }
 }
 const viewAllDepartments = async () => {
     try {
         const [data] = await db.promise().query('SELECT * from department')
+        console.table(data);
+        menu()
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const createDepartment = async () => {
+    try {
+        const [data] = await db.promise().query('INSERT INTO department')
         console.table(data);
         menu()
     } catch (err) {
@@ -39,9 +58,29 @@ const viewAllEmployees = async () => {
     }
 }
 
+const createEmployee = async () => {
+    try {
+        const [data] = await db.promise().query('INSERT INTO employee')
+        console.table(data);
+        menu()
+    } catch (err) {
+        console.log(err)
+    }
+}
+
 const viewAllRoles = async () => {
     try {
         const[data] = await db.promise().query('SELECT role.title, role.salary,department.name from role left join department on role.department_id = department.id')
+        console.table(data);
+        menu()
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const createRole = async () => {
+    try {
+        const[data] = await db.promise().query('INSERT INTO role')
         console.table(data);
         menu()
     } catch (err) {
